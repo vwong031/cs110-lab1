2fae52bf5a5126c1f96ff1108aeb1f9d63dd005c
Jenny Quan <toxic_dreamer@MacBook-Air-8.lan>
2024-04-20 16:22:14 -0700

    display O's, player turn, added score count labels

--- lab3/main.js
+++ lab3/main.js
@@ -1,7 +1,35 @@
+var currentPlayer = "X";
+var openSpots = 9;
+var xScore = 0;
+var yScore = 0;
+var xMoves = [];
+var oMoves = [];
+
 function Play(id) {
   var playerChoice = document.querySelector("." + id + " .xo");
-  playerChoice.innerHTML = "X";
+  playerChoice.innerHTML = currentPlayer;
+
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+  var currentPlayerName = currentPlayer === "X" ? "Player X" : "Player O";
+  document.querySelector(".display_player").textContent = currentPlayerName;
+  openSpots --;
+
+  if (winCheck()) {
+    if (currentPlayer == "X")
+    {
+      xScore++;
+    } else {
+      yScore++;
+    }
+  }
+
+  if (openSpots == 0)
+  {
+    alert("Out of moves")
+  }
 }
+
 
 function Reset() {
   var playerChoice = document.getElementsByClassName("xo");
@@ -9,4 +37,8 @@ function Reset() {
   for (var i = 0; i < playerChoice.length; ++i) {
     playerChoice[i].innerHTML = '';
   }
+}
+
+function winCheck(){
+
 }
