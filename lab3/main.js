279537c61337fcb4a54afa4c48bdb09ae2576b9c
Val <vwong031@ucr.edu>
2024-04-21 17:00:23 -0700

    update code such that you can no longer click on & update a box that already has an X or O in it

--- lab3/main.js
+++ lab3/main.js
@@ -8,37 +8,46 @@ var oMoves = [];
 
 function Play(id) {
   var playerChoice = document.querySelector("." + id + " .xo");
-  playerChoice.innerHTML = currentPlayer;
-
   
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-
-  
-  var currentPlayerName = currentPlayer === "X" ? "Player X" : "Player O";
-  document.querySelector(".display_player").textContent = currentPlayerName;
-  openSpots--;
-
-  var className = row; 
-  if (currentPlayer === "X") {
-    xMoves.push(className);
-  } else {
-    oMoves.push(className);
-  }
-
-  if (winCheck()) {
-    if (currentPlayer == "X")
-    {
-      xScore++;
-      document.querySelector(".display-score-X").textContent = xScore;
-    } else {
-      yScore++;
-      document.querySelector(".display-score-Y").textContent = yScore;
-    }
-  }
-
-  if (openSpots == 0)
+  if (openSpots <= 0)
   {
     alert("Out of moves")
+  }
+
+  // Check if the box already contains an X or O
+  if (playerChoice.textContent === '') {
+    playerChoice.innerHTML = currentPlayer;
+
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+    document.querySelector("." + id).style.backgroundColor = "lightcoral";
+  
+    var currentPlayerName = currentPlayer === "X" ? "Player X" : "Player O";
+    document.querySelector(".display_player").textContent = currentPlayerName;
+    openSpots--;
+
+    className = row; 
+    if (currentPlayer === "X") {
+      xMoves.push(className);
+    } else {
+      oMoves.push(className);
+    }
+  
+    if (winCheck()) {
+      if (currentPlayer == "X")
+      {
+        xScore++;
+        document.querySelector(".display-score-X").textContent = xScore;
+      } else {
+        yScore++;
+        document.querySelector(".display-score-Y").textContent = yScore;
+      }
+    }
+  
+    if (openSpots == 0)
+    {
+      alert("Out of moves")
+    }
   }
 }
 
