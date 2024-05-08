a2e970e1da09130cf5dace4329ba5c306605d04c
Val <vwong031@ucr.edu>
2024-05-08 16:24:46 -0700

    fixed article title & publication date spacing

--- lab5/src/components/Articles.js
+++ lab5/src/components/Articles.js
@@ -10,14 +10,22 @@ const Articles = ({ articles }) => {
     <div className="news-container">
       {articles.map((article, index) => (
         <div className="news" key={index}>
-          <h3>{`${index + 1}) ${article.title}`}</h3>
-          <div className="publication-date">{article.published_date}</div>
-          <div className="article-description">
-            {/* Add conditional rendering for nested properties */}
-            {article.media && article.media[0] && article.media[0]['media-metadata'] && article.media[0]['media-metadata'][0] && (
-              <img src={article.media[0]['media-metadata'][0].url} alt={article.title} />
-            )}
-            <div className="article-sentence">{article.abstract.split('.')[0]}</div>
+          <div class="title">
+            <h3 class="titleDiv">
+              {`${index + 1}) ${article.title}`}
+            </h3>
+            <div className="publication-date">
+              {article.published_date}
+            </div>
+          </div>
+          <div class="articleContent">
+            <div className="article-description">
+              {/* Add conditional rendering for nested properties */}
+              {article.media && article.media[0] && article.media[0]['media-metadata'] && article.media[0]['media-metadata'][0] && (
+                <img src={article.media[0]['media-metadata'][0].url} alt={article.title} />
+              )}
+              <div className="article-sentence">{article.abstract.split('.')[0]}</div>
+            </div>
           </div>
         </div>
       ))}
