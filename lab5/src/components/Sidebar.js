9a3b0dfbf06cc895f5d2cb7edef6f15f6baf1e66
Val <vwong031@ucr.edu>
2024-05-07 17:42:42 -0700

    style sidebar. input & search button do not work yet

--- lab5/src/components/Sidebar.js
+++ lab5/src/components/Sidebar.js
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import fetchArticles from './Articles.js';
+import './Sidebar.css'
 
 const Sidebar = ({ fetchArticlesOnChange }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div>
+    <div class="sidebar">
       <form id="filterForm">
-        <div>
-          <input type="radio" name="timeFrame" value="day" onChange={fetchArticlesOnChange} /> Day
-          <input type="radio" name="timeFrame" value="week" onChange={fetchArticlesOnChange} /> Week
-          <input type="radio" name="timeFrame" value="month" onChange={fetchArticlesOnChange} /> Month
+        <div class="input-and-button">
+          <input class="input" type="text" placeholder="Enter a value 1-15" value={searchTerm} onChange={handleInputChange} />
+          <button class="button">
+            Search
+          </button>
         </div>
+        <p class="filterTitle">
+          Sort By:
+        </p>
+        <div class="options-group">
+          <div className="option">
+            <input type="radio" name="sortBy" value="viewed" onChange={fetchArticlesOnChange} />
+            <label htmlFor="viewed">Most Viewed</label>
+          </div>
+          <div className="option">
+            <input type="radio" name="sortBy" value="shared" onChange={fetchArticlesOnChange} />
+            <label htmlFor="shared">Most Shared</label>
+          </div>
+          <div className="option">
+            <input type="radio" name="sortBy" value="emailed" onChange={fetchArticlesOnChange} />
+            <label htmlFor="emailed">Most Emailed</label>
+          </div>
+        </div>
+
         <br />
-        <div>
-          <input type="radio" name="sortBy" value="viewed" onChange={fetchArticlesOnChange} /> Viewed
-          <input type="radio" name="sortBy" value="shared" onChange={fetchArticlesOnChange} /> Shared
-          <input type="radio" name="sortBy" value="emailed" onChange={fetchArticlesOnChange} /> Emailed
+
+        <p class="filterTitle">
+          Time Frame:
+        </p>
+        <div class="options-group">
+          <div className="option">
+            <input type="radio" name="timeFrame" value="day" onChange={fetchArticlesOnChange} />
+            <label htmlFor="day">Day</label>
+          </div>
+          <div className="option">
+            <input type="radio" name="timeFrame" value="week" onChange={fetchArticlesOnChange} />
+            <label htmlFor="week">Week</label>
+          </div>
+          <div className="option">
+            <input type="radio" name="timeFrame" value="month" onChange={fetchArticlesOnChange} />
+            <label htmlFor="month">Month</label>
+          </div>
         </div>
       </form>
     </div>
