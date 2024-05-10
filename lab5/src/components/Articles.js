4c6fe853b32ca2ca33ef9046dd04f30e99b2cbd2
Jenny Quan <toxic_dreamer@MacBook-Air-8.lan>
2024-05-10 01:48:11 -0700

    added article errors but messed up article numbers

--- lab5/src/components/Articles.js
+++ lab5/src/components/Articles.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import './Articles.css'
+import './Articles.css';
 
 const Articles = ({ articles }) => {
-  const [currentPage, setCurrentPage] = useState(1);  // Tracks the page it is at
-  let articlesPerPage = 6;  // Determine how many articles can be displayed in a page
+  const [currentPage, setCurrentPage] = useState(1); // Tracks the page it is at
+  const articlesPerPage = 6; // Determine how many articles can be displayed in a page
   let lastPageArticles = articlesPerPage;
   
   if (currentPage === 3) {
@@ -21,64 +21,51 @@ const Articles = ({ articles }) => {
   const pageFirstArticle = pageLastArticle - lastPageArticles;
   const currentArticles = articles.slice(pageFirstArticle, pageLastArticle);
 
-  const oddArticles = currentArticles.filter((_, index) => index % 2 === 0);
-  const evenArticles = currentArticles.filter((_, index) => index % 2 !== 0);
-
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  if (!articles) {
-    return <div>No articles yet</div>; 
-  }
+  const renderArticle = (article, index) => {
+    try {
+      const articleNumber = index + 1;
+  
+      return (
+        <div className="news" key={index}>
+          <div className="title">
+            <h3 className="titleDiv">{`${articleNumber}) ${article.title}`}</h3>
+            <div className="publication-date">{article.published_date}</div>
+          </div>
+          <div className="articleContent">
+            <div className="article-description">
+              {article.media && article.media[0] && article.media[0]['media-metadata'] && article.media[0]['media-metadata'][0] && (
+                <img className="article-img" src={article.media[0]['media-metadata'][0].url} alt={article.title} />
+              )}
+              <div className="article-sentence">{article.abstract.match(/.*?\./)[0]}</div>
+            </div>
+          </div>
+        </div>
+      );
+    } catch (error) {
+      console.error('Error rendering article:', error);
+      return (
+        <div className="news" key={index}>
+          <div className="title">
+            <h3 className="titleDiv">Article not available</h3>
+          </div>
+          <div className="articleContent">
+            <div className="article-description">
+              <div className="article-sentence">Article information could not be retrieved.</div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };  
+  
 
   return (
     <div className="news-container">
       <div className="columns">
-        <div className="column">
-          {oddArticles.map((article, index) => (
-           <div className="news" key={index}>
-            <div className="title">
-              <h3 className="titleDiv">
-                {`${pageFirstArticle + index * 2 + 1}) ${article.title}`}
-              </h3>
-              <div className="publication-date">
-                {article.published_date}
-              </div>
-            </div>
-            <div className="articleContent">
-              <div className="article-description">
-                {article.media && article.media[0] && article.media[0]['media-metadata'] && article.media[0]['media-metadata'][0] && (
-                  <img className="article-img" src={article.media[0]['media-metadata'][0].url} alt={article.title} />
-                )}
-                <div className="article-sentence">{article.abstract.match(/.*?\./)[0]}</div>
-              </div>
-            </div>
-          </div>
-          ))}
-        </div>
-
-        <div className="column">
-          {evenArticles.map((article, index) => (
-           <div className="news" key={index}>
-            <div className="title">
-              <h3 className="titleDiv">
-                {`${pageFirstArticle + index * 2 + 2}) ${article.title}`}
-              </h3>
-              <div className="publication-date">
-                {article.published_date}
-              </div>
-            </div>
-            <div className="articleContent">
-              <div className="article-description">
-                {article.media && article.media[0] && article.media[0]['media-metadata'] && article.media[0]['media-metadata'][0] && (
-                  <img className="article-img" src={article.media[0]['media-metadata'][0].url} alt={article.title} />
-                )}
-                <div className="article-sentence">{article.abstract && article.abstract.match(/.*?\./) && article.abstract.match(/.*?\./)[0]}
-</div>
-              </div>
-            </div>
-          </div>
-          ))}
-        </div>
+        <div className="column">{currentArticles.filter((_, index) => index % 2 === 0).map(renderArticle)}</div>
+        <div className="column">{currentArticles.filter((_, index) => index % 2 !== 0).map(renderArticle)}</div>
       </div>
 
       <div className="pagination">
